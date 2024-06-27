ad9543fb8510a67c3f08df2cf46be4c175010f6d
Ivan Vulović <ivan@Ivans-MacBook-Pro.local>
2024-06-27 21:56:49 +0200

    init

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-# Future
+# xtml
 
-Static Website Generator
+The Evolution of Static.
 
 Built entirely with HTML, CSS and Vanilla JS.
 
-The goal is to have a simple framework that will output pure HTML at the end.
+At the end, you'll get fully static HTML/CSS website with minimal JS.
+
+---
 
 ### Pages:
 
@@ -27,11 +29,33 @@ Allow passing props (data) to the template for customization.
 Act as blueprints for your widgets, defining the overall structure.
 Can display and manipulate props passed from the widget for dynamic content. Template can use components and receive props.
 
----
-
 ### In essence:
 
 You build pages by composing components and reusable widgets.
 Widgets are defined by templates and customized with props. 
 
 This modular approach promotes clean, maintainable, and scalable static websites.
+
+---
+
+### WHY ?
+
+Because i needed something that generates pure html without bunch of JS chunks all around.
+ 
+### DEVELOPMENT
+
+In order to run this app in development you need to hit only one comand:
+
+`docker-compose up --build`
+
+It will start development server at http://localhost.
+
+You will see dist folder with all the HTMLs generated inside. 
+
+If you are not using docker, and you don't wanna serve it via nginx, you can simply build the app by running `node main.js` and open generated dist file via filesystem protocol.
+
+### CONTRIBUTION
+
+**Your contribution is very welcome!**
+
+Feel free to create a pull request.
--- src/public/script.js
+++ src/public/script.js
@@ -1,152 +1,5 @@
 'use strict';
 
-class SidebarManager {
-  constructor(sidebarSelector, sidebarBtnSelector) {
-    this.sidebar = document.querySelector(sidebarSelector);
-    this.sidebarBtn = document.querySelector(sidebarBtnSelector);
-    this.setupEventListeners();
-  }
-
-  setupEventListeners() {
-    this.sidebarBtn.addEventListener("click", () => {
-      this.toggleSidebar();
-    });
-  }
-
-  toggleSidebar() {
-    this.sidebar.classList.toggle("active");
-  }
-}
-
-class TestimonialsModal {
-  constructor(testimonialsItemsSelector, modalContainerSelector, modalCloseBtnSelector, overlaySelector) {
-    this.testimonialsItems = document.querySelectorAll(testimonialsItemsSelector);
-    this.modalContainer = document.querySelector(modalContainerSelector);
-    this.modalCloseBtn = document.querySelector(modalCloseBtnSelector);
-    this.overlay = document.querySelector(overlaySelector);
-
-    this.modalImg = document.querySelector("[data-modal-img]");
-    this.modalTitle = document.querySelector("[data-modal-title]");
-    this.modalText = document.querySelector("[data-modal-text]");
-
-    this.setupEventListeners();
-  }
-
-  setupEventListeners() {
-    this.testimonialsItems.forEach(item => {
-      item.addEventListener("click", () => {
-        this.fillModal(item);
-        this.toggleModal();
-      });
-    });
-
-    this.modalCloseBtn.addEventListener("click", () => {
-      this.toggleModal();
-    });
-
-    this.overlay.addEventListener("click", () => {
-      this.toggleModal();
-    });
-  }
-
-  fillModal(item) {
-    this.modalImg.src = item.querySelector("[data-testimonials-avatar]").src;
-    this.modalImg.alt = item.querySelector("[data-testimonials-avatar]").alt;
-    this.modalTitle.innerHTML = item.querySelector("[data-testimonials-title]").innerHTML;
-    this.modalText.innerHTML = item.querySelector("[data-testimonials-text]").innerHTML;
-  }
-
-  toggleModal() {
-    this.modalContainer.classList.toggle("active");
-    this.overlay.classList.toggle("active");
-  }
-}
-
-class FilterManager {
-  constructor(selectSelector, selectItemSelector, selectValueSelector, filterBtnSelector) {
-    this.selectElement = document.querySelector(selectSelector);
-    this.selectItems = document.querySelectorAll(selectItemSelector);
-    this.selectValueElement = document.querySelector(selectValueSelector);
-    this.filterButtons = document.querySelectorAll(filterBtnSelector);
-    this.filterItems = document.querySelectorAll("[data-filter-item]");
-
-    this.initializeEvents();
-  }
-
-  initializeEvents() {
-    this.selectElement.addEventListener("click", this.toggleDropdown.bind(this)); // Bind 'this' context
-
-    this.selectItems.forEach(item => {
-      item.addEventListener("click", () => this.handleItemSelected(item));
-    });
-
-    this.filterButtons.forEach(button => {
-      button.addEventListener("click", () => this.handleFilterButtonClick(button));
-    });
-  }
-
-  elementToggleFunc(elem) {
-    elem.classList.toggle("active");
-  }
-
-  toggleDropdown() {
-    this.elementToggleFunc(this.selectElement);
-  }
-
-  handleItemSelected(selectedItem) {
-    const selectedValue = selectedItem.innerText.toLowerCase();
-    this.selectValueElement.innerText = selectedValue;
-    this.toggleDropdown();
-    this.filterFunc(selectedValue);
-  }
-
-  handleFilterButtonClick(selectedButton) {
-    const selectedValue = selectedButton.innerText.toLowerCase();
-    this.selectValueElement.innerText = selectedValue;
-    this.filterFunc(selectedValue);
-    this.updateActiveFilterButton(selectedButton);
-  }
-
-  updateActiveFilterButton(selectedButton) {
-    this.filterButtons.forEach(btn => btn.classList.remove('active'));
-    selectedButton.classList.add("active");
-  }
-
-  filterFunc(selectedValue) {
-    this.filterItems.forEach(item => {
-      if (selectedValue === "all") {
-        item.classList.add("active");
-      } else if (selectedValue === item.dataset.category) {
-        item.classList.add("active");
-      } else {
-        item.classList.remove("active");
-      }
-    });
-  }
-}
-
-class ContactForm {
-  constructor(formSelector, formInputsSelector, formBtnSelector) {
-    this.form = document.querySelector(formSelector);
-    this.formInputs = document.querySelectorAll(formInputsSelector);
-    this.formBtn = document.querySelector(formBtnSelector);
-
-    this.setupEventListeners();
-  }
-
-  setupEventListeners() {
-    this.formInputs.forEach(input => {
-      input.addEventListener("input", () => {
-        if (this.form.checkValidity()) {
-          this.formBtn.removeAttribute("disabled");
-        } else {
-          this.formBtn.setAttribute("disabled", "");
-        }
-      });
-    });
-  }
-}
-
 class Router {
   constructor(navigationLinksSelector, pagesSelector) {
     this.navigationLinks = document.querySelectorAll(navigationLinksSelector);
@@ -157,34 +10,23 @@ class Router {
   }
 
   handleInitialHash() {
-    const currentHash = window.location.hash.slice(1).toLowerCase();
-    const currentPath = window.location.pathname.slice(1).toLocaleLowerCase();
-    if (currentHash) {
-      this.activatePage(currentHash);
-    }
+    const currentPath = this.trim(window.location.pathname);
+
     if (currentPath) {
-      this.activatePage(currentPath);
+      this.activateLink(currentPath);
     }
-    if (!currentHash && !currentPath) {
-      this.activatePage("home");
+    if (!currentPath) {
+      this.activateLink("home");
     }
   }
 
-  activatePage(pageName) {
+  activateLink(pageName) {
     this.navigationLinks.forEach(link => {
       const linkText = link.textContent.trim().toLowerCase();
-      // const linkTextHTML = `${linkText}.html`;
-      const correspondingPage = document.querySelector(`[data-page="${linkText}"]`);
       if (linkText === pageName) {
         link.classList.add("active");
-        if (correspondingPage) {
-          correspondingPage.classList.add("active");
-        }
       } else {
         link.classList.remove("active");
-        if (correspondingPage) {
-          correspondingPage.classList.remove("active");
-        }
       }
     });
   }
@@ -203,33 +45,11 @@ class Router {
       const hash = this.trim(event.target.location.hash);
       const pathname = this.trim(event.target.location.pathname);
       if (hash) {
-        this.activatePage(hash);
+        this.activateLink(hash);
       }
       if (pathname) {
-        this.activatePage(pathname)
+        this.activateLink(pathname)
       }
-    });
-
-    this.navigationLinks.forEach((link, index) => {
-      link.addEventListener("click", e => {
-        // e.preventDefault();
-        const pageName = link.textContent.trim().toLowerCase();
-        this.pages.forEach((page, pageIndex) => {
-          if (pageName === page.dataset.page) {
-            page.classList.add("active");
-            this.navigationLinks[pageIndex].classList.add("active");
-            window.scrollTo(0, 0);
-            if (pageIndex === 0) {
-              history.pushState("", document.title, window.location.pathname);
-            } else {
-              window.location.hash = pageName;
-            }
-          } else {
-            page.classList.remove("active");
-            this.navigationLinks[pageIndex].classList.remove("active");
-          }
-        });
-      });
     });
   }
 }
@@ -316,80 +136,7 @@ class FileExtensionManager {
     this.findLinks();
     this.removeHtmlExtension();
   }
-}
-// /** WIDGETS */
-// try{
-
-//   class Component {
-//     constructor(element) {
-//       this.element = element;
-//       const { component, props } = this.getComponentData(element);
-//       this.componentName = component;
-//       this.props = props;
-//     }
-
-//     // Function to get component data (unchanged)
-//     getComponentData(element) {
-//       const component = element.dataset.component;
-//       const props = {};
-//       for (const [key, value] of Object.entries(element.dataset)) {
-//         if (key.startsWith('prop')) {
-//           const propName = key.slice(4).toLowerCase(); // Remove "prop-" prefix and lowercase
-//           props[propName] = value;
-//         }
-//       }
-//       return { component, props };
-//     }
-
-//     render() {
-//       // Implement component rendering logic based on componentName and props
-//       let content;
-//       switch (this.componentName) {
-//         case 'wallet':
-//           content = `<h2>Wallet with ID: ${this.props.id}</h2>`;
-//           break;
-//         // Add more cases for other component types
-//         default:
-//           content = `Unknown component: ${this.componentName}`;
-//       }
-//       this.element.innerHTML = content;
-//     }
-//   }
-
-
-
-//   // Function to render components (now within the class)
-//   class App {
-//     constructor() {
-//       this.appElement = document.getElementById('app');
-//     }
-//     // this should be invoked at build:
-//     /**
-//      * - get all components
-//      * 
-//      * - render them at specific place, for example <!-- component: wallet -->
-//      */
-//     renderComponents() {
-//       const elements = this.appElement.querySelectorAll('[data-component]');
-//       elements.forEach(element => {
-//         const component = new Component(element);
-//         component.render();
-//       });
-//     }
-
-//     start() {
-//       this.renderComponents();
-//     }
-//   }
-
-//   // Create and start the app
-//   const app = new App();
-//   app.start();
-// } catch(e){
-//   console.log(e)
-// }
-// /** WIDGETS END */
-
+} 
 
 try{
   const manager = new OnLoadManager();
@@ -401,66 +148,12 @@ try{
 document.addEventListener("DOMContentLoaded", () => {
   const navLinkSelector = "[data-nav-link]";
   const pageSelector = "[data-page]";
-  const formSelector = "[data-form]";
-  const formInputSelector = "[data-form-input]";
-  const formButtonSelector = "[data-form-btn]";
-  const sidebarSelector = "[data-sidebar]";
-  const sidebarButtonSelector = "[data-sidebar-btn]";
-  const testimonialItemSelector = "[data-testimonials-item]";
-  const modalContainerSelector = "[data-modal-container]";
-  const modalCloseButtonSelector = "[data-modal-close-btn]";
-  const overlaySelector = "[data-overlay]";
-
-  const blogSelectSelector = "[data-select-blog]";
-  const blogSelectItemSelector = "[data-select-item-blog]";
-  const blogSelectValueSelector = "[data-selecct-value-blog]";
-  const blogFilterButtonSelector = "[data-filter-btn-blog]";
-
-  const portfolioSelectSelector = "[data-select-portfolio]";
-  const portfolioSelectItemSelector = "[data-select-item-portfolio]";
-  const portfolioSelectValueSelector = "[data-selecct-value-portfolio]";
-  const portfolioFilterButtonSelector = "[data-filter-btn-portfolio]";
-
-  const photographySelectSelector = "[data-select-photography]";
-  const photographySelectItemSelector = "[data-select-item-photography]";
-  const photographySelectValueSelector = "[data-selecct-value-photography]";
-  const photographyFilterButtonSelector = "[data-filter-btn-photography]";
+  
 
   try {
     new Router(navLinkSelector, pageSelector);
   } catch (e) {
     console.log("Error in Router: ", e);
-  }
-  try {
-    new ContactForm(formSelector, formInputSelector, formButtonSelector);
-  } catch (e) {
-    console.log("Error in ContactForm: ", e);
-  }
-  try {
-    new SidebarManager(sidebarSelector, sidebarButtonSelector);
-  } catch (e) {
-    console.log("Error in SidebarManager: ", e);
-  }
-  try {
-    new TestimonialsModal(testimonialItemSelector, modalContainerSelector, modalCloseButtonSelector, overlaySelector);
-  } catch (e) {
-    console.log("Error in TestimonialsModal: ", e);
-  }
-  try {
-    new FilterManager(blogSelectSelector, blogSelectItemSelector, blogSelectValueSelector, blogFilterButtonSelector);
-  } catch (e) {
-    console.log("Error in BlogFilterManager: ", e);
-  }
-  try {
-    new FilterManager(portfolioSelectSelector, portfolioSelectItemSelector, portfolioSelectValueSelector, portfolioFilterButtonSelector);
-  } catch (e) {
-    console.log("Error in PortfolioFilterManager: ", e);
-  }
-
-  try {
-    new FilterManager(photographySelectSelector, photographySelectItemSelector, photographySelectValueSelector, photographyFilterButtonSelector);
-  } catch (e) {
-    console.log("Error in PhotographyFilterManager: ", e);
   }
 
   try {
